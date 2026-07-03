7a51737948e3b4944a0be10efb7e25b3ef6b4267
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-07-03 10:34:23 +0530

    Time: 13 ms (100%), Space: 160.1 MB (84.16%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.java
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.java
@@ -1,98 +1,73 @@
 class Solution {
+    private int[] head, next, to, weight;
+    private long k;
+    private int n;
     public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
-    int n = online.length;
+        this.n = online.length;
+        this.k = k;
 
-        List<int[]>[] graph = new ArrayList[n];
-        for (int i = 0; i < n; i++)
-            graph[i] = new ArrayList<>();
+        int m = edges.length;
+        this.head = new int[n];
+        this.next = new int[m];
+        this.to = new int[m];
+        this.weight = new int[m];
+        Arrays.fill(head, -1);
+        int left = Integer.MAX_VALUE, right = 0;
+        for(int i = 0; i < m; i++) {
+            int a = edges[i][0], b = edges[i][1], c = edges[i][2];
+            if(online[a] && online[b]) {
+                to[i] = b;
+                next[i] = head[a];
+                weight[i] = c;
+                head[a] = i;
 
-        int[] indegree = new int[n];
-
-        int maxEdge = 0;
-
-        for (int[] e : edges) {
-            graph[e[0]].add(new int[]{e[1], e[2]});
-            indegree[e[1]]++;
-            maxEdge = Math.max(maxEdge, e[2]);
-        }
-
-        // Topological order
-        int[] topo = new int[n];
-        Queue<Integer> q = new ArrayDeque<>();
-
-        for (int i = 0; i < n; i++)
-            if (indegree[i] == 0)
-                q.offer(i);
-
-        int idx = 0;
-
-        while (!q.isEmpty()) {
-            int u = q.poll();
-            topo[idx++] = u;
-
-            for (int[] e : graph[u]) {
-                if (--indegree[e[0]] == 0)
-                    q.offer(e[0]);
+                if(c > right) right = c;
+                if(c < left) left = c;
             }
         }
 
-        int lo = 0;
-        int hi = maxEdge;
-        int ans = -1;
+        if(!check(0)) return -1;
 
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-
-            if (check(mid, graph, topo, online, k)) {
-                ans = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
+        while(left < right) {
+            int mid = left + right + 1 >>> 1;
+            if(check(mid)) left = mid;
+            else right = mid - 1;
         }
-
-        return ans;
+        return left;
     }
 
-    private boolean check(int limit,
-                          List<int[]>[] graph,
-                          int[] topo,
-                          boolean[] online,
-                          long k) {
-
-        int n = online.length;
-
-        long INF = Long.MAX_VALUE / 4;
-
+    private static final int[] queue = new int[50001];
+    private static final long[] sum = new long[50001];
+    private boolean check(int threshold) {
         long[] dist = new long[n];
-        Arrays.fill(dist, INF);
-
+        Arrays.fill(dist, k + 1);
         dist[0] = 0;
 
-        for (int u : topo) {
-
-            if (dist[u] == INF)
-                continue;
-
-            if (u != 0 && u != n - 1 && !online[u])
-                continue;
-
-            for (int[] e : graph[u]) {
-
-                int v = e[0];
-                int w = e[1];
-
-                if (w < limit)
-                    continue;
-
-                if (v != n - 1 && !online[v])
-                    continue;
-
-                if (dist[v] > dist[u] + w)
-                    dist[v] = dist[u] + w;
+        int read = 0, write = 1;
+        while(read < write) {
+            int current = queue[read];
+            long val = sum[read++];
+            if(val > dist[current]) continue;
+            for(int i = head[current]; i != -1; i = next[i]) {
+                if(weight[i] < threshold) continue;
+                int nextIndex = to[i];
+                long d = val + weight[i];
+                if(d < dist[nextIndex]) {
+                    if(nextIndex == n - 1) return true;
+                    dist[nextIndex] = d;
+                    sum[write] = d;
+                    queue[write++] = nextIndex;
+                }
             }
         }
-
-        return dist[n - 1] <= k;
+        return false;
+    }
+    private static final class Node {
+        private final int index;
+        private final long dist;
+        private Node(int index, long dist) {
+            this.index = index;
+            this.dist = dist;
+        }
     }
 }
