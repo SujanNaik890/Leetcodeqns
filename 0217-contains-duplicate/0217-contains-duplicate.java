2379bb322478ddaf56e3db13c33ba3d2e26f86c5
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-07-09 17:53:13 +0530

    Time: 23 ms (28.16%), Space: 92.9 MB (74.55%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+        return !(nums.length==set.size());
+    }
+}
