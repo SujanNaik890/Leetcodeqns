0c874505a2c1736467c672d845124ec76289c3bd
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-07-19 11:15:26 +0530

    Time: 2 ms (98.45%), Space: 43 MB (47.01%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -1,26 +1,47 @@
+// class Solution {
+//     public String smallestSubsequence(String s) {
+        //  int[] last = new int[26];
+        // for (int i = 0; i < s.length(); i++) {
+        //     last[s.charAt(i) - 'a'] = i;
+        // }
+        // boolean[] visited = new boolean[26];
+        // Stack<Character> stack = new Stack<>();
+        // for (int i = 0; i < s.length(); i++) {
+        //   char ch = s.charAt(i);
+        //     if (visited[ch - 'a'])
+        //         continue;
+        //     while (!stack.isEmpty()
+        //             && ch < stack.peek()
+        //             && last[stack.peek() - 'a'] > i) {
+        //         visited[stack.pop() - 'a'] = false;
+        //     }
+        //     stack.push(ch);
+        //     visited[ch - 'a'] = true;
+        // }
+        // StringBuilder ans = new StringBuilder();
+        // for (char c : stack)
+        //     ans.append(c);
+        // return ans.toString(); 
 class Solution {
-    public String smallestSubsequence(String s) {
-         int[] last = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            last[s.charAt(i) - 'a'] = i;
-        }
-        boolean[] visited = new boolean[26];
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-          char ch = s.charAt(i);
-            if (visited[ch - 'a'])
-                continue;
-            while (!stack.isEmpty()
-                    && ch < stack.peek()
-                    && last[stack.peek() - 'a'] > i) {
-                visited[stack.pop() - 'a'] = false;
-            }
-            stack.push(ch);
-            visited[ch - 'a'] = true;
-        }
-        StringBuilder ans = new StringBuilder();
-        for (char c : stack)
-            ans.append(c);
-        return ans.toString();
+    public String smallestSubsequence(String text) { 
+        StringBuilder sb = new StringBuilder(); 
+        int[] count = new int[128];
+        boolean[] used = new boolean[128]; 
+        for (final char c : text.toCharArray()) ++count[c]; 
+        for (final char c : text.toCharArray()) {
+             --count[c]; if (used[c]) continue;
+              while (sb.length() > 0 && last(sb) > c && count[last(sb)] > 0) { 
+                used[last(sb)] = false;
+                sb.setLength(sb.length() - 1);
+            } 
+            used[c] = true; 
+            sb.append(c); 
+        } return sb.toString();
+     } 
+     private char last(StringBuilder sb) { 
+        return sb.charAt(sb.length() - 1); 
     }
 }
+     
+             
+    
