f7586d6539532afb5df48192e5b9b5391732df5d
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-07-20 18:17:05 +0530

    Time: 8 ms (45.73%), Space: 47 MB (62.88%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.java
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -1,13 +1,21 @@
 class Solution {
-    public int dayOfYear(String s) {
-        int[] m = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
-        int y=Integer.parseInt(s.substring(0, 4));
-        boolean tell=(y%400==0||(y%4==0&&y%100!=0));
-         int a=s.charAt(5)-'0';
-         a=a*10+s.charAt(6)-'0';
-         int b=s.charAt(8)-'0';
-         b=b*10+s.charAt(9)-'0';
-         if(tell&&a>2) return m[a-1]+b+1;
-         return m[a-1]+b;
+    public int dayOfYear(String date) {
+        int feb=0;
+        int ans=0;
+        if(Integer.parseInt(date.substring(0,4))%4==0){
+            feb=29;
+        }
+        else{
+            feb=28;
+        }
+        int[] year = {31,feb,31,30,31,30,31,31,30,31,30,31};
+        int m= Integer.parseInt(date.substring(5,7));
+        for(int i=0;i<m-1;i++){
+            ans=ans+year[i];
+        }
+        if(Integer.parseInt(date.substring(0,4))==1900){
+            return ans+ Integer.parseInt(date.substring(8)) -1;
+        }
+        return ans+ Integer.parseInt(date.substring(8));
     }
 }
