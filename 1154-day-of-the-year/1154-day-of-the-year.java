6eaf2aff86784a1cdebf928163c9533238130753
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-07-20 18:16:34 +0530

    Time: 7 ms (96.95%), Space: 47 MB (62.88%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.java
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -1,25 +1,13 @@
 class Solution {
-    public int dayOfYear(String date) {
-        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        
-        int year = Integer.parseInt(date.substring(0, 4));
-        int month = Integer.parseInt(date.substring(5, 7));
-        int day = Integer.parseInt(date.substring(8, 10));
-
-        if (isLeapYear(year)) {
-            days[1] = 29;
-        }
-
-        int totalDays = 0;
-        for(int i = 0; i < month - 1; i++){
-            totalDays += days[i];
-        }
-        totalDays += day;
-
-        return totalDays;
-    }
-
-    boolean isLeapYear(int year){
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    public int dayOfYear(String s) {
+        int[] m = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+        int y=Integer.parseInt(s.substring(0, 4));
+        boolean tell=(y%400==0||(y%4==0&&y%100!=0));
+         int a=s.charAt(5)-'0';
+         a=a*10+s.charAt(6)-'0';
+         int b=s.charAt(8)-'0';
+         b=b*10+s.charAt(9)-'0';
+         if(tell&&a>2) return m[a-1]+b+1;
+         return m[a-1]+b;
     }
 }
