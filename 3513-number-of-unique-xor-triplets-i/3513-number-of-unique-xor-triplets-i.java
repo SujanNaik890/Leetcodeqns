fedf8c7cfe735b0b5e71c764cc85ed899f600d41
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-07-23 13:15:41 +0530

    Time: 1 ms (51.02%), Space: 125.4 MB (53.06%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -1,10 +1,16 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        if(nums.length<=2)return nums.length;
-        int mask=0;
-        for(int num:nums){
-            mask|=num;
-        }
-        return mask+1;
+        // if(nums.length<=2)return nums.length;
+        // int mask=0;
+        // for(int num:nums){
+        //     mask|=num;
+        // }
+        // return mask+1;
+
+        int n = nums.length;
+        
+        return 1 << (32 - Integer.numberOfLeadingZeros(n) - 3 / (n + 1));
+    
+
     }
 }
