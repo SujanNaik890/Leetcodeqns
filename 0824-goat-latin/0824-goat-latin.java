a559458415d5079236a2c6385e8d1f5ee83f05ff
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-08-13 21:46:51 +0530

    Time: 2 ms (83.06%), Space: 43 MB (84.47%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.java
+++ 0824-goat-latin/0824-goat-latin.java
@@ -1,15 +1,24 @@
 class Solution {
     public String toGoatLatin(String sentence) {
-        StringBuilder sb = new StringBuilder();
         String[] words = sentence.split(" ");
-        int r = 1;
-        for(String word : words){
-            char ch = word.charAt(0);
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-                sb.append(word).append("ma").append("a".repeat(r++)).append(" ");
-            else
-                sb.append(word.substring(1,word.length())).append(word.charAt(0)).append("ma").append("a".repeat(r++)).append(" ");            
-        }        
-        return sb.toString().trim();
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for (String word : words) {
+            char first = word.charAt(0);
+            switch (first) {
+                case 'a': case 'e': case 'i': case 'o': case 'u':
+                case 'A': case 'E': case 'I': case 'O': case 'U':
+                    sb.append(word); break;
+                default:
+                    sb.append(word.substring(1));
+                    sb.append(first);
+            }
+            sb.append("ma");
+            for (int i = 0; i < count; i++) sb.append("a");
+            sb.append(" ");
+            count++;
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
 }
