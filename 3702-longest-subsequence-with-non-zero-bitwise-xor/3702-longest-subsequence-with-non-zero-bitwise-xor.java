bba6d45a9578361025f552a2181c0199655ba061
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-08-15 10:38:27 +0530

    Time: 2 ms (68.38%), Space: 133.7 MB (18.38%) - LeetHub

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.java
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.java
@@ -5,7 +5,7 @@ class Solution {
         for(int x:nums){
             nonzero|=x>0;
             longseq^=x;
-        }
+        }//HAPPY INDEPENDANCE DAY
         if(!nonzero)return 0;
         return longseq==0? nums.length-1:nums.length;
     }
