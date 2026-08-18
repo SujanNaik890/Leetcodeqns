fd3b2ece26543473e1390dc599e7bc7c9a718b9c
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-08-18 22:19:53 +0530

    Time: 1 ms (99.3%), Space: 44.6 MB (74.74%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,3 +1,69 @@
+/*class Solution {
+    public int largestInteger(int[] nums, int k) {
+        Map<Integer, int[]> map = new HashMap<>();
+        int largestInt = -1;
+
+        int[] subarray;
+
+        for (int i = 0; i <= nums.length - k; i++) {
+            subarray = Arrays.copyOfRange(nums, i, k + i);
+
+            map.put(i, subarray);
+        }
+
+        Map<Integer, Integer> frqTable = getFrequency(nums, map);
+        if (frqTable.size() == 1 && k < 2)
+            return largestInt;
+
+        int minFrq = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, Integer> entry : frqTable.entrySet()) {
+        
+            minFrq = Math.min(entry.getValue(), minFrq);
+        }
+
+        ArrayList<Integer> smallestFreq = new ArrayList<Integer>();
+
+        for (Map.Entry<Integer, Integer> entry : frqTable.entrySet()) {
+            if (entry.getValue() == 1) {
+                smallestFreq.add(entry.getKey());
+            }
+        }
+
+        for (int n : smallestFreq) {
+            largestInt = Math.max(largestInt, n);
+        }
+
+        return largestInt;
+    }
+
+    public Map<Integer, Integer> getFrequency(int[] nums, Map<Integer, int[]> map) {
+        Map<Integer, Integer> freqTable = new HashMap<>();
+        
+        
+        
+        if(map.size() == 1) {
+            Set<Integer> uniqueNumbers = new HashSet<>();
+            
+            int[] subarray = map.get(0);
+            for (int number : subarray) {
+                uniqueNumbers.add(number);
+            }
+            for (int number : uniqueNumbers) {
+                freqTable.merge(number, 1, Integer::sum);
+            }
+            return freqTable;
+            
+        }
+        
+       for (int[] subarray : map.values()){
+        for (int number : subarray) {
+            freqTable.merge(number, 1, Integer::sum);
+        }}
+    
+
+    return freqTable;
+
+}}*/
 class Solution {
     public int largestInteger(int[] nums, int k) {
         int n = nums.length;
